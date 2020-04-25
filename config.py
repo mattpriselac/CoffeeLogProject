fc62ab71ba94976da1b564e860f9789d707ba2bf
MP <matt@MPs-MacBook-Pro.local>
2020-04-25 17:06:14 -0500

    Changed the theme and added color

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQALCHEMY_TRACK_MODIFICATIONS = False
+#    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+#    SQALCHEMY_TRACK_MODIFICATIONS = False
