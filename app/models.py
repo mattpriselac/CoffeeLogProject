3f2d9cf205a1eb60f725d6ceefb1638a7a1ecc1a
MP <matt@MPs-MacBook-Pro.local>
2020-04-28 17:54:27 -0500

    add  login system

--- app/models.py
+++ app/models.py
@@ -1,46 +1,26 @@
 from datetime import datetime
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import login
 
-class GreenCoffee(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    source = db.Column(db.String(128), index=True, unique=False)
-    date_acquired = db.Column(db.DateTime, index=True, unique=False)
-    origin_country = db.Column(db.String(60), index=True, unique=False)
-    farm_information = db.Column(db.String(256), index=False, unique=False)
-    official_notes = db.Column(db.String(256), index=False, unique=False)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_has = db.Column(db.String(128))
+
+    def set_password(self, password):
+        self.password_has = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_has, password)
+
+
 
     def __repr__(self):
-        return '<{} from {}>'.format(self.origin_country, self.source)
+        return '<User {}>'.format(self.username)
 
-class RoastedCoffee(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    roaster = db.Column(db.String(128), index=True)
-    roast_date = db.Column(db.DateTime, index=True)
-    coffee_name = db.Column(db.String(60))
-    origin_country = db.Column(db.String(60))
-    farm_information = db.Column(db.String(256))
-    official_notes = db.Column(db.String(256))
-    roast_session = db.relationship('RoastSession', backref='roast_info', lazy='dynamic')
-
-    def __repr__(self):
-        return '<{} from {}>'.format(self.coffee_name, self.roaster)
-
-class TastingSession(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    coffee = db.Column(db.Integer, db.ForeignKey('roastedcoffee.id'))
-    prep_method = db.Column(db.String(50), index=True, unique=False)
-    coffee_in = db.Column(db.Integer, index=False, unique=False)
-    liquid_in = db.Column(db.Integer, index=False, unique=False)
-    beverage_out = db.Column(db.Integer, index=False, unique=False)
-    tasting_notes = db.Column(db.String(200), index=False, unique=False)
-    coffee = db.relationship('RoastedCoffee', backref='coffee', lazy='dynamic')
-
-class RoastSession(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    green_coffee = db.Column(db.Integer, db.ForeignKey('greencoffee.id'))
-    green_weight = db.Column(db.Integer, index=False, unique=False)
-    roasted_weight = db.Column(db.Integer, index=False, unique=False)
-    roast_time = db.Column(db.Integer, index=False, unique=False)
-    fc_time = db.Column(db.Integer, index=False, unique=False)
-    temp_data = db.Column(db.String(256), index=False, unique=False)
-    green_coffee = db.relationship('GreenCoffee', backref='greens', lazy='dynamic')
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
--- app/routes.py
+++ app/routes.py
@@ -1,25 +1,32 @@
 from app import app
 from flask import render_template, url_for, redirect, flash
-from app.forms import NewGreenCoffee, NewRoastedCoffee
+from app.forms import LoginForm
+from flask_login import current_user, login_user, logout_user, login_required
+from app.models import User
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     return render_template('index.html', title='Home')
 
 @app.route('/new_event')
+@login_required
 def new_event():
     return render_template('new_event.html')
 
 @app.route('/new_tasting')
+@login_required
 def new_tasting():
     return render_template('new_tasting.html')
 
 @app.route('/green_coffees')
+@login_required
 def green_coffees():
     return render_template('green_coffees.html')
 
 @app.route('/new_green_coffee', methods=['GET','POST'])
+@login_required
 def new_green_coffee():
     form = NewGreenCoffee()
     if form.validate_on_submit():
@@ -28,10 +35,12 @@ def new_green_coffee():
     return render_template('new_green_coffee.html', title="New Green Coffee", form=form)
 
 @app.route('/new_roast')
+@login_required
 def new_roast():
     return render_template('new_roast.html')
 
 @app.route('/new_roasted_coffee', methods=['GET','POST'])
+@login_required
 def new_roasted_coffee():
     form = NewRoastedCoffee()
     if form.validate_on_submit():
@@ -41,13 +50,35 @@ def new_roasted_coffee():
 
 
 @app.route('/roasted_coffees')
+@login_required
 def roasted_coffees():
     return render_template('roasted_coffees.html')
 
 @app.route('/roasts')
+@login_required
 def roasts():
     return render_template('roasts.html')
 
 @app.route('/tastings')
+@login_required
 def tastings():
     return render_template('tastings.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
